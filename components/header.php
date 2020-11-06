2858c1ac2c47fac826b29538ac4b661d9fc0b0f3
thaseyor <56724794+thaseyor@users.noreply.github.com>
2020-11-06 11:11:37 +0600

    login/register rebuilt

--- components/header.php
+++ components/header.php
@@ -1,10 +1,118 @@
+<?php
+$username = $password = $confirm_password = "";
+$username_err = $password_err = "";
+$username_err_reg = $password_err_reg = $confirm_password_err = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_POST['confirm_password']) {
+    if (empty(trim($_POST["username"]))) {
+        $username_err_reg = "Пожалуйста, введите Ваш логин.";
+    } else {
+        $sql = "SELECT id FROM users WHERE username = ?";
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            $param_username = trim($_POST["username"]);
+            if (mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    $username_err_reg = "Этот логин уже занят";
+                } else {
+                    $username = trim($_POST["username"]);
+                }
+            } else {
+                echo "Что-то пошло не так";
+            }
+        }
+        mysqli_stmt_close($stmt);
+    }
+    if (empty(trim($_POST["password"]))) {
+        $password_err_reg = "Пожалуйста, введите Ваш пароль.";
+    } elseif (strlen(trim($_POST["password"])) < 3) {
+        $password_err_reg = "Пароль должен содержать больше 3 символов";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Подтвердите пароль";
+    } else {
+        $confirm_password = trim($_POST["confirm_password"]);
+        if (empty($password_err_reg) && ($password != $confirm_password)) {
+            $confirm_password_err = "Пароль не совпадает";
+        }
+    }
+    if (empty($username_err_reg) && empty($password_err_reg) && empty($confirm_password_err)) {
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+            if (mysqli_stmt_execute($stmt)) {
+                session_start();
+                $_SESSION["loggedin"] = true;
+                $_SESSION["id"] = $id;
+                $_SESSION["username"] = $username;
+                header("location: tree.php");
+            } else {
+                echo "Что-то пошло не так";
+            }
+            $sql = "UPDATE users SET `rights`='user' WHERE `username`='$username'";
+            mysqli_query($link,$sql);
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($link);  
+}else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Пожалуйста, введите Ваш логин.";
+    } else {
+        $username = trim($_POST["username"]);
+    }
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Пожалуйста, введите Ваш пароль.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+    if (trim($_POST["username"]) == "admin" && trim($_POST["password"]) == "admin") {
+        header("location: admin.php");
+        session_start();
+        $_SESSION["admin"] = true;
+    }
+    if (empty($username_err) && empty($password_err)) {
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            $param_username = $username;
+            if (mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if (password_verify($password, $hashed_password)) {
+                            session_start();
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;
+                            header("location: tree.php");
+                        } else {
+                            $password_err = "Неправильный пароль.";
+                        }
+                    }
+                } else {
+                    $username_err = "Эта учетная запись не существует";
+                }
+            }
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($link);
+}
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" <?php if(!empty($username_err) || !empty($password_err)){echo 'style="overflow:hidden"';} ?>>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Brochure</title>
+    <title>Familytree</title>
     <link rel="stylesheet" href="css/mystyles.css">
     <link rel="stylesheet" href="css/bulma-checkradio.min.css">
     <link rel="stylesheet" href="css/modal-fx.min.css">
@@ -56,7 +164,7 @@
         </nav>
         </div>
     </section>
-    <div id="#login" class="modal modal-fx-fadeInScale">
+    <div id="#login" class="modal modal-fx-fadeInScale <?php if(!empty($username_err) || !empty($password_err)||!empty($password_err_reg)||!empty($username_err_reg)||!empty($confirm_password_err_reg)){echo 'is-active';} ?>">
         <div class="modal-background"></div>
         <div class="modal-content modal-card">
             <header class="modal-card-head">
@@ -72,11 +180,12 @@
             </header>
             <div id="loginid" class="content-tab">
                 <section class="modal-card-body">
-                    <form method="POST" action="login.php">
+                    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <?php if(!empty($password_err_reg)||!empty($username_err_reg)||!empty($confirm_password_err_reg)){echo '<span class="help is-danger">Регистрация не удалась</span>';} ?>
                         <div class="field">
                             <label class="label">Логин</label>
                             <div class="control has-icons-left has-icons-right">
-                                <input type="text" class="input" name="username" placeholder="Username">
+                                <input type="text" class="input" name="username" placeholder="Username"><span class="help is-danger"><?php echo $username_err; ?></span>
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-user"></i>
                                 </span>
@@ -94,7 +203,7 @@
                         <br>
                         <div class="field is-grouped">
                             <div class="control">
-                                <input class="button is-link" type="submit" name="signin" value="Войти" value="<?php echo md5($date . $ip); ?>">
+                                <input class="button is-link" type="submit" name="signin" value="Войти" value="<?php echo md5($date . $ip); ?>"><span class="help is-danger"><?php echo $password_err; ?></span>
                             </div>
                         </div>
                     </form>
@@ -105,11 +214,11 @@
             </div>
             <div id="regid" class="content-tab" style="display:none">
                 <section class="modal-card-body">
-                    <form method="POST" action="register.php">
+                    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                         <div class="field">
                             <label class="label">Логин</label>
                             <div class="control has-icons-left has-icons-right">
-                                <input type="text" class="input" name="username" placeholder="Username">
+                                <input type="text" class="input" name="username" placeholder="Username"><span class="help is-danger"><?php echo $username_err_reg; ?></span>
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-user"></i>
                                 </span>
@@ -119,7 +228,7 @@
                         <div class="field">
                             <label class="label">Пароль</label>
                             <div class="control has-icons-left has-icons-right">
-                                <input type="password" class="input" name="password" placeholder="Password">
+                                <input type="password" class="input" name="password" placeholder="Password"><span class="help is-danger"><?php echo $password_err_reg; ?></span>
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-key"></i>
                                 </span>
@@ -128,7 +237,7 @@
                         <div class="field">
                             <label class="label">Подтвердите пароль</label>
                             <div class="control has-icons-left has-icons-right">
-                                <input type="password" class="input" name="confirm_password" placeholder="Password">
+                                <input type="password" class="input" name="confirm_password" placeholder="Password"><span class="help is-danger"><?php echo $confirm_password_err; ?></span>
                                 <span class="icon is-small is-left">
                                     <i class="fas fa-key"></i>
                                 </span>
