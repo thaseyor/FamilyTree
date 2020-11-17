a0b85e1624aa5059837150cb91e4383cc0803ca3
thaseyor <56724794+thaseyor@users.noreply.github.com>
2020-11-17 16:36:31 +0600

    Update main.php

--- components/main.php
+++ components/main.php
@@ -1,8 +1,11 @@
 <section class="hero is-medium" style="background: rgb(255,255,255);
 background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(223,233,243,1) 100%);">
     <div class="hero-body">
-        <div class="container "><p class="title"><span style="font-size: 3.5rem">
-        На этом сайте вы можете получить информацию о лучших местах для посещения, опасностях и т.д.</span></p>
+        <div class="container ">
+        <figure class="image is-4by5">
+        <img src='https://ds05.infourok.ru/uploads/ex/0f30/000b6d57-1f244e15/hello_html_mfbb96db.png'>
+</figure>
+
         </div>
     </div>
 </section>
